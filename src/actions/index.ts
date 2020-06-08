1731dce08fdbaadd6780bd4226222375426e7006
Alediran <55855331+Alediran@users.noreply.github.com>
2020-06-08 14:16:50 -0300

    Project completed

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,28 +1,4 @@
-import axios from 'axios';
-import 'redux';
-import { Dispatch } from 'redux';
-import { ActionTypes } from './types';
+export * from './types'; //Actions Types
 
-interface Todo {
-  id: number;
-  title: string;
-  completed: boolean;
-}
-
-interface FetchTodosAction {
-  type: ActionTypes.fetchTodos;
-  payload: Todo[];
-}
-
-const url = 'https://jsonplaceholder.typicode.com/todos';
-
-export const fetchTodos = () => {
-  return async (dispatch: Dispatch) => {
-    const response = await axios.get<Todo[]>(url);
-
-    dispatch<FetchTodosAction>({
-      type: ActionTypes.fetchTodos,
-      payload: response.data,
-    });
-  };
-};
+//List of
+export * from './todos';
